59f78ddb517b4d503b46b02b8d27eeb4d7fe61f2
Blue-Onion <rawatadityasingh963@gmail.com>
2026-03-19 20:40:33 +0530

    feat: implement graceful server shutdown to handle termination signals.

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Blue-Onion/RestApi-Go/config"
 	"github.com/go-chi/chi"
@@ -10,19 +15,31 @@ import (
 
 func main() {
 	//Load Env
-	config:=config.LoadConfig()
+	config := config.LoadConfig()
 
 	//Server
-	router:=chi.NewRouter()
-	server:=http.Server{
+	router := chi.NewRouter()
+	server := http.Server{
 		Handler: router,
-		Addr: ":"+config.Port,
+		Addr:    ":" + config.Port,
 	}
 	go func() {
-		err:=server.ListenAndServe()
-		if err!=nil{
+		log.Printf("Server Listenting on http:/localhost:%s\n", config.Port)
+		err := server.ListenAndServe()
+		if err != nil {
 			log.Fatal(err.Error())
 		}
 
 	}()
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	err := server.Shutdown(ctx)
+	if err != nil {
+		log.Fatalf("Server forced to shutdown: %s", err.Error())
+	}
+	log.Printf("Server Shutdowned\n")
+
 }

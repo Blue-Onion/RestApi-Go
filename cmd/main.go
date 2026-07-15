c392cde5634d22b176b69d479ab30adfa1eed021
Blue-Onion <rawatadityasingh963@gmail.com>
2026-07-15 18:11:22 +0530

    Scratch

--- cmd/main.go
+++ cmd/main.go
@@ -9,72 +9,45 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/Blue-Onion/RestApi-Go/config"
-	"github.com/Blue-Onion/RestApi-Go/handler"
-	"github.com/Blue-Onion/RestApi-Go/handler/auth"
-	"github.com/Blue-Onion/RestApi-Go/middleware"
-
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 )
 
+const PORT = "3480"
+
 func main() {
-	//Load Env
-	cfg := config.LoadConfig()
-
-	//DB
-	apiCfg, err := config.DbQuries()
-	if err != nil {
-		log.Fatalf("Couldn't connect to database: %v", err)
-	}
-
-	//Handlers
-	authHandler := &auth.Handler{
-		Repo: apiCfg.UserRepo,
-	}
-	middlewareHandler := &middleware.Handler{
-		Repo: apiCfg.UserRepo,
-	}
-
-	//Server
 	router := chi.NewRouter()
-	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://*", "https://*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"*"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300,
-	}))
-	router.Use(middleware.MiddlewareRateLimit)
-	router.Get("/health", handler.Health)
-	router.Get("/", handler.MainPage)
-	authRouter := auth.AuthRoute(authHandler, *middlewareHandler)
-	router.Mount("auth", authRouter)
 
+	router.Use(cors.Handler(
+		cors.Options{
+			AllowedOrigins:   []string{"https://*", "http://*"},
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
+			AllowedHeaders:   []string{"*"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: true,
+			MaxAge:           300,
+		}))
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte("hello"))
+	})
 	server := http.Server{
 		Handler: router,
-		Addr:    ":" + cfg.Port,
+		Addr:    ":" + PORT,
 	}
-
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
-
 	go func() {
-		log.Printf("Listening on http://localhost:%s", cfg.Port)
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Printf("Listening on http://localhost:%s", PORT)
+		if err := server.ListenAndServe(); err != nil {
 			log.Fatalf("Error occurred: %v", err)
 		}
 	}()
-
 	<-stop
 	log.Println("Shutting down server...")
-
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
-
 	if err := server.Shutdown(ctx); err != nil {
 		log.Fatalf("Error occurred in Shutdown: %v", err)
 	}
-	log.Println("Server Shutdown gracefully")
 }
